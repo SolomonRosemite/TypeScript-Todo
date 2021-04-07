de8e95f6e888b80e74b94f6c6b7dcf00fda587b8
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2021-04-07 15:10:49 +0200

    🚀Released version v1.4.6

--- todo-list/client/src/extension.ts
+++ todo-list/client/src/extension.ts
@@ -59,53 +59,87 @@ export function activate(context: ExtensionContext) {
       "**/{Library,node_modules,dist,build,.next}/**"
     )
     .then((files) => {
-      // Foreach textDocument highlight todo
+      // Foreach textDocument highlight actions
       for (let i = 0; i < files.length; i++) {
         workspace.openTextDocument(files[i]).then((textDocument) => {
-          let text = textDocument.getText();
-
-          const values = GetRightLanguage(textDocument.languageId);
-          let patternV2 = RegExp(values[0], "g");
-
-          let m: RegExpExecArray | null;
-          let diagnostics: Diagnostic[] = [];
-
-          while ((m = patternV2.exec(text.toUpperCase()))) {
-            const end = new Position(
-              textDocument.positionAt(m.index).line,
-              Number.MAX_VALUE - 1
+          validateTextDocument(
+            textDocument,
+            "TODO",
+            DiagnosticSeverity.Information,
+            []
+          )
+            .then((values) =>
+              validateTextDocument(
+                textDocument,
+                "BUG",
+                DiagnosticSeverity.Warning,
+                values
+              )
+            )
+            .then((values) =>
+              validateTextDocument(
+                textDocument,
+                "NOTE",
+                DiagnosticSeverity.Hint,
+                values
+              )
             );
-
-            const range = new Range(textDocument.positionAt(m.index), end);
-
-            const line = textDocument.getText(range);
-            let diagnostic: Diagnostic = {
-              severity: DiagnosticSeverity.Information,
-              range: range,
-              message: line.substring(parseInt(values[1])).trimRight(),
-              source: "Rosemite",
-              code: "todo",
-            };
-            diagnostics.push(diagnostic);
-          }
-          client.diagnostics.set(textDocument.uri, diagnostics);
         });
       }
     });
+}
 
-  function GetRightLanguage(languageId: string): string[] {
-    switch (languageId) {
-      case "python":
-        return ["# TODO*", "2"];
-      case "yaml":
-        return ["# TODO*", "2"];
-      case "html":
-        return ["<!-- TODO*", "0"];
-      case "lua":
-        return ["-- TODO*", "0"];
-      default:
-        return ["// TODO*", "2"];
-    }
+async function validateTextDocument(
+  textDocument: TextDocument,
+  keyWord: string,
+  type: DiagnosticSeverity,
+  prev: Diagnostic[]
+): Promise<Diagnostic[]> {
+  const text = textDocument.getText();
+
+  const values = GetRightLanguage(textDocument.languageId, keyWord);
+  let patternV2 = RegExp(values[0], "g");
+
+  let m: RegExpExecArray | null;
+  let diagnostics: Diagnostic[] = [];
+
+  while ((m = patternV2.exec(text.toUpperCase()))) {
+    const end = new Position(
+      textDocument.positionAt(m.index).line,
+      Number.MAX_VALUE - 1
+    );
+
+    const range = new Range(textDocument.positionAt(m.index), end);
+
+    const line = textDocument.getText(range);
+    let diagnostic: Diagnostic = {
+      severity: DiagnosticSeverity.Information,
+      range: range,
+      message: line.substring(parseInt(values[1])).trimRight(),
+      source: "Rosemite",
+      code: "todo",
+    };
+    diagnostics.push(diagnostic);
+  }
+  client.diagnostics.set(textDocument.uri, diagnostics);
+  return diagnostics;
+}
+
+// Returns RegExp for current used Language
+function GetRightLanguage(languageId: string, keyWord: string): string[] {
+  switch (languageId) {
+    case "python":
+      return [`# ${keyWord}`, "2"];
+    case "powershell":
+      return [`# ${keyWord}`, "2"];
+    case "yaml":
+      return [`# ${keyWord}`, "2"];
+    case "html":
+      return [`<!-- ${keyWord}`, "0"];
+    case "lua":
+      return [`-- ${keyWord}`, "0"];
+    default:
+      return [`// ${keyWord}`, "2"];
   }
 }
 
